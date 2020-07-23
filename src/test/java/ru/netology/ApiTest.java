12f631b798214f748a7623640396ee5833b09a69
Ander1977 <kopa.andrej@gmail.com>
2020-07-23 20:15:43 +0300

    Changed method

--- src/test/java/ru/netology/ApiTest.java
+++ src/test/java/ru/netology/ApiTest.java
@@ -1,38 +1,17 @@
 package ru.netology;
 
-
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static io.restassured.RestAssured.given;
+
+import static ru.netology.DataGenerator.RegistrationInfo.generateUserInfo;
+import static ru.netology.DataGenerator.RegistrationInfo.setUpUser;
 
 public class ApiTest {
-    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .addFilter(new ResponseLoggingFilter())
-            .log(LogDetail.ALL)
-            .build();
-
-    private void setUpUser(UserInfo user) {
-        given()
-                .spec(requestSpec)
-                .body(user)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
-    }
 
     private void loginPage(String login, String password) {
         open("http://localhost:9999");
@@ -41,33 +20,33 @@ public class ApiTest {
         $("[data-test-id=action-login]").click();
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void shouldUserActive() {
-        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", false);
+        UserInfo user = generateUserInfo("ru", false);
         setUpUser(user);
         loginPage(user.getLogin(), user.getPassword());
         $(byText("Личный кабинет")).waitUntil(visible, 15000);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void shouldUserBlocked() {
-        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", true);
+        UserInfo user = generateUserInfo("ru", true);
         setUpUser(user);
         loginPage(user.getLogin(), user.getPassword());
         $(withText("Пользователь заблокирован")).waitUntil(visible, 15000);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void shouldUsernameNotCorrect() {
-        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", false);
+        UserInfo user = generateUserInfo("ru", false);
         setUpUser(user);
         loginPage(DataGenerator.RegistrationInfo.userName("ru"), user.getPassword());
         $(withText("Неверно указан логин или пароль")).waitUntil(visible, 15000);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void shouldPasswordNotCorrect() {
-        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", false);
+        UserInfo user = generateUserInfo("ru", false);
         setUpUser(user);
         loginPage(user.getLogin(), DataGenerator.RegistrationInfo.userPassword("ru"));
         $(withText("Неверно указан логин или пароль")).waitUntil(visible, 15000);
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,7 +1,16 @@
 package ru.netology;
 
 import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
 import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
 
 public class DataGenerator {
     private DataGenerator() {
@@ -22,10 +31,30 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUserInfo(String Locale, boolean isBlocked) {
-            return new UserInfo(
+            UserInfo info = new UserInfo(
                     userName(Locale),
                     userPassword(Locale),
                     (isBlocked) ? "blocked" : "active");
+            return new UserInfo();
         }
+
+        public static void setUpUser(UserInfo user) {
+            given()
+                    .spec(requestSpec)
+                    .body(user)
+                    .when()
+                    .post("/api/system/users")
+                    .then()
+                    .statusCode(200);
+        }
+
+        private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+                .setBaseUri("http://localhost")
+                .setPort(9999)
+                .setAccept(ContentType.JSON)
+                .setContentType(ContentType.JSON)
+                .addFilter(new ResponseLoggingFilter())
+                .log(LogDetail.ALL)
+                .build();
     }
 }
